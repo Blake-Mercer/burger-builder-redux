f2dff70e88679ae8994aee17e59476144c2aee44
Blake Mercer <blake.mercer93@gmail.com>
2020-11-10 10:57:15 -0500

    set up initial file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <div> whats good!</div>
-    </div>
-  );
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
